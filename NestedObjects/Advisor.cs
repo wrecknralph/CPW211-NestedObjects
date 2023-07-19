8d74d29919a0b5f965bf5f7d0251595ec10974d7
wrecknralph <thompson.bradley@comcast.net>
2023-07-18 21:28:49 -0700

    Added missing documentation and student date of birth

--- NestedObjects/Advisor.cs
+++ NestedObjects/Advisor.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace NestedObjects
 {
-    internal class Advisor
+    /// <summary>
+    /// Represents a college advisor
+    /// </summary>
+    public class Advisor
     {
+        /// <summary>
+        /// The legal first and last name
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// The advisors work email address
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Building and room number of the office
+        /// </summary>
+        public string OfficeLocation { get; set; }
+
+        public Advisor AssignedAdvisor { get; set; }
     }
 }
--- NestedObjects/Student.cs
+++ NestedObjects/Student.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace NestedObjects
 {
-    internal class Student
+    /// <summary>
+    /// Represents a individual college student
+    /// </summary>
+    public class Student
     {
+        /// <summary>
+        /// The legal first name
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// The legal last name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The assigned school email address
+        /// </summary>
+        public string SchoolEmail { get; set; }
+
+        /// <summary>
+        /// The date of birth
+        /// </summary>
+        public DateOnly DateOfBirth { get; set; }
+
+        /// <summary>
+        /// Students preferred contact number
+        /// </summary>
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// The advisor who is assigned to the student
+        /// </summary>
+        public Advisor AssignedAdvisor { get; set; }
     }
 }
